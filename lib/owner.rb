c4a25e178293ca04f0dabbcf1710813177ddde21
johncban <johncban@gmail.com>
2019-05-25 02:31:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -77,7 +77,7 @@ class Owner
   end
 
   def lists_pets
-    "I have #{@pets[:fish].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)"
+    "I have #{@pets[:fish].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
 end
